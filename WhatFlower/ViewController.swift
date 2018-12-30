44741c4de904ae30221944e8f2ec3145038fccec
nozomu.kuwae <kuwae@1-10.com>
2018-12-30 21:52:05 +0900

    Initial Commit

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Nozomu Kuwae on 2018/12/30.
+//  Copyright © 2018 NKCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
