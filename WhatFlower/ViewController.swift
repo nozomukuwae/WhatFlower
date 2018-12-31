21640fc05f20a0ee0883e3a5ca10d0cd1859d48d
nozomu.kuwae <kuwae@1-10.com>
2018-12-31 17:01:31 +0900

    Add image recognition function

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+        
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { fatalError("Error getting image from image picker") }
+        imageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        guard let ciimage = CIImage(image: image) else {
+            fatalError("Error loading image")
+        }
+        
+        detect(ciImage: ciimage)
+    }
+    
+    private func detect(ciImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Error loading CoreML Model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error getting result")
+            }
+            
+            print(results)
+            
+            if let firstResult = results.first {
+                DispatchQueue.main.async {
+                    self.navigationItem.title = firstResult.identifier.capitalized
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
 
